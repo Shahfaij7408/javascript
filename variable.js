0f00e10b938354c7de793cf443ee43a82cc2d865
Shahfaij7408 <ansarishahfaij@gmail.com>
2025-04-13 15:20:23 +0000

    commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+const acccountId= 123456
+let accountEmail= "js@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+// acccountId= 345
+accountEmail= 'an'
+accountPassword='faiz'
+accountCity='kolkata'
+
+console.log(accountCity);
+console.table([acccountId, accountEmail, accountPassword, accountCity])
+
